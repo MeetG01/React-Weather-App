06ff81e7e1d8bd8d6e1fda90ec3acd55328d818d
Meet Gala <meetgala012@gmail.com>
2023-05-04 10:35:20 +0530

    firwst commit

--- src/api.js
+++ src/api.js
@@ -1,11 +1,11 @@
 export const geoApiOptions = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": "85215052c6msh6ccb43c6956e957p191ff5jsn5e3a6547e143",// enter your rapid api key here
+    "X-RapidAPI-Key": "",// enter your rapid api key here
     "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
   },
 };
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
-export const WEATHER_API_KEY = "afbe0bef325f5156e2487166c090ba67"; // enter your key from openweather API
+export const WEATHER_API_KEY = ""; // enter your key from openweather API
